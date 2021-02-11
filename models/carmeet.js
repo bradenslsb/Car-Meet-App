36b9fa43e6aa07a2c81a5661b556be20fd84eed3
bradenslsb <bbir1199@gmail.com>
2021-02-11 11:57:10 -0700

    wrong size dis

--- models/carmeet.js
+++ models/carmeet.js
@@ -6,7 +6,7 @@ const carmeetSchema = new mongoose.Schema({
   uid: String,
   isPast: Boolean,
   price: Number,
-  desc: { type: String, maxlength: 5000},
+  desc: { type: String, maxlength: 6000},
   date: { type: Date},
   country: String,
   region: String,
